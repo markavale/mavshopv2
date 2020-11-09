656d8b83ad3a438e816cd7c7022caf99b2564975
Mark Anthony Vale <markavale1@gmail.com>
2020-11-10 02:18:43 +0800

    production

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 # from django.conf import settings
 # from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('users.urls')),
     path('api/', include('analytics.urls')),
